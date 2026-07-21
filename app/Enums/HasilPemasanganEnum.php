62773fac772296ccac0936a8553358310229111a
zulalhafizh <ahmadzulfikaralhafizh@gmail.com>
2026-07-21 09:07:45 +0700

    refactor: hapus entitas dan flow jadwal survey & pemasangan lama

--- app/Enums/HasilPemasanganEnum.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Enums;
-
-enum HasilPemasanganEnum: string
-{
-    case SELESAI = 'selesai';
-    case DITUNDA = 'ditunda';
-}
--- app/Enums/HasilSurveyEnum.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Enums;
-
-enum HasilSurveyEnum: string
-{
-    case BERHASIL = 'berhasil';
-    case KENDALA = 'kendala';
-}
